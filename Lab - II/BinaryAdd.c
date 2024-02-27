1952a9d3123f28e79cc26611b19f8232023f4024
comethrusws <basabjha@naver.com>
2024-02-27 08:47:39 +0545

    Lab - II

--- Lab - II/BinaryAdd.c
+++ Lab - II/BinaryAdd.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-// Function to add two 4-bit binary numbers
 void addBinary4Bit(char binary1[], char binary2[], char result[]) {
     int carry = 0;
 
@@ -15,7 +14,6 @@ void addBinary4Bit(char binary1[], char binary2[], char result[]) {
         carry = (bit1 & bit2) | (bit2 & carry) | (carry & bit1);
     }
 
-    // If there is a carry after the addition, set the most significant bit
     if (carry)
         result[0] = '1';
 }
@@ -23,17 +21,14 @@ void addBinary4Bit(char binary1[], char binary2[], char result[]) {
 int main() {
     char binary1[5], binary2[5], result[5];
 
-    // Input two 4-bit binary numbers
     printf("Enter the first 4-bit binary number: ");
     scanf("%s", binary1);
 
     printf("Enter the second 4-bit binary number: ");
     scanf("%s", binary2);
 
-    // Perform binary addition
     addBinary4Bit(binary1, binary2, result);
 
-    // Display the result
     printf("The sum of %s and %s is: %s\n", binary1, binary2, result);
 
     return 0;
